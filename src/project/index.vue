d86968018d850865b22b6e8cf93e65a4aca7cc81
Alen <webwork-lsl@qq.com>
2021-02-20 15:31:28 +0800

    fix: 解决移动端1px边框

--- src/project/index.vue
+++ src/project/index.vue
@@ -5,6 +5,8 @@
     <ADD></ADD>
     <p>-----------------------------</p>
     <SUB></SUB>
+    <p>-----------------------------</p>
+    <div class="one-pixel-border"></div>
   </div>
 </template>
 <script lang="ts">
@@ -17,4 +19,21 @@ import SUB from "./storeTst/store-sub.vue";
 })
 export default class extends Vue {}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.one-pixel-border {
+  position: relative;
+  box-sizing: border-box;
+}
+
+.one-pixel-border::before {
+  display: block;
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 200%;
+  height: 200%;
+  border: 1px solid red;
+  transform: translate(-50%, -50%) scale(0.5, 0.5);
+}
+</style>
